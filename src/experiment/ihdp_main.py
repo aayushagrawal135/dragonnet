2247f8c011391debd14ff875eaedf9af121f7d3d
Aayush Agrawal <aayushagrawal135@gmail.com>
2022-04-15 19:13:03 -0400

    Remove nednet, tarnet, setup MNIST for dragonnet

--- src/experiment/ihdp_main.py
+++ src/experiment/ihdp_main.py
@@ -1,5 +1,9 @@
-from experiment.models import *
+# test
 import os
+import numpy as np
+import math
+
+from models import *
 import glob
 import argparse
 import tensorflow as tf
@@ -8,10 +12,10 @@ from sklearn.model_selection import train_test_split
 import keras.backend as K
 from tensorflow.keras.optimizers import RMSprop, SGD, Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau, TerminateOnNaN
-from experiment.idhp_data import *
+from idhp_data import *
 
 
-def _split_output(yt_hat, t, y, y_scaler, x, index):
+def _split_output(yt_hat, t, y, y_scaler, x):
     q_t0 = y_scaler.inverse_transform(yt_hat[:, 0].reshape(-1, 1).copy())
     q_t1 = y_scaler.inverse_transform(yt_hat[:, 1].reshape(-1, 1).copy())
     g = yt_hat[:, 2].copy()
@@ -26,23 +30,26 @@ def _split_output(yt_hat, t, y, y_scaler, x, index):
                                                                         g[t.squeeze() == 0.].mean())
     print(var)
 
-    return {'q_t0': q_t0, 'q_t1': q_t1, 'g': g, 't': t, 'y': y, 'x': x, 'index': index, 'eps': eps}
+    return {'q_t0': q_t0, 'q_t1': q_t1, 'g': g, 't': t, 'y': y, 'x': x, 'eps': eps}
 
 
-def train_and_predict_dragons(t, y_unscaled, x, targeted_regularization=True, output_dir='',
+def train_and_predict_dragons(t_train, t_test, y_train, y_test, x_train, x_test, targeted_regularization=True,
+                              output_dir='',
                               knob_loss=dragonnet_loss_binarycross, ratio=1., dragon='', val_split=0.2, batch_size=64):
     verbose = 0
-    y_scaler = StandardScaler().fit(y_unscaled)
-    y = y_scaler.transform(y_unscaled)
-    train_outputs = []
-    test_outputs = []
+    y_train = y_train.reshape(-1, 1)
+    y_test = y_test.reshape(-1, 1)
+    x_train = x_train.reshape(x_train.shape[0], -1)
+    x_test = x_test.reshape(x_test.shape[0], -1)
+    t_train = t_train.reshape(t_train.shape[0], -1)
+    t_test = t_test.reshape(t_test.shape[0], -1)
 
-    if dragon == 'tarnet':
-        dragonnet = make_tarnet(x.shape[1], 0.01)
+    y_scaler = StandardScaler().fit(y_train)
+    y_train = y_scaler.transform(y_train)
+    y_test = y_scaler.transform(y_test)
 
-    elif dragon == 'dragonnet':
-        print("I am here making dragonnet")
-        dragonnet = make_dragonnet(x.shape[1], 0.01)
+    print("I am here making dragonnet")
+    dragonnet = make_dragonnet(x_train.shape[1], 0.01)
 
     metrics = [regression_loss, binary_classification_loss, treatment_accuracy, track_epsilon]
 
@@ -51,25 +58,13 @@ def train_and_predict_dragons(t, y_unscaled, x, targeted_regularization=True, ou
     else:
         loss = knob_loss
 
-    # for reporducing the IHDP experimemt
-
-    i = 0
-    tf.random.set_seed(i)
-    np.random.seed(i)
-    train_index, test_index = train_test_split(np.arange(x.shape[0]), test_size=0.2, random_state=1)
-    test_index = train_index
-
-    x_train, x_test = x[train_index], x[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    t_train, t_test = t[train_index], t[test_index]
-
     yt_train = np.concatenate([y_train, t_train], 1)
 
-    import time;
+    import time
     start_time = time.time()
 
     dragonnet.compile(
-        optimizer=Adam(learning_rate = 1e-3),
+        optimizer=Adam(learning_rate=1e-3),
         loss=loss, metrics=metrics)
 
     adam_callbacks = [
@@ -77,7 +72,6 @@ def train_and_predict_dragons(t, y_unscaled, x, targeted_regularization=True, ou
         EarlyStopping(monitor='val_loss', patience=2, min_delta=0.),
         ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, verbose=verbose, mode='auto',
                           min_delta=1e-8, cooldown=0, min_lr=0)
-
     ]
 
     dragonnet.fit(x_train, yt_train, callbacks=adam_callbacks,
@@ -92,10 +86,11 @@ def train_and_predict_dragons(t, y_unscaled, x, targeted_regularization=True, ou
                           min_delta=0., cooldown=0, min_lr=0)
     ]
 
-    sgd_lr = 1e-5
+    sgd_lr = 1e-6
     momentum = 0.9
-    dragonnet.compile(optimizer=SGD(learning_rate = sgd_lr, momentum=momentum, nesterov=True), loss=loss,
+    dragonnet.compile(optimizer=SGD(learning_rate=sgd_lr, momentum=momentum, nesterov=True), loss=loss,
                       metrics=metrics)
+
     dragonnet.fit(x_train, yt_train, callbacks=sgd_callbacks,
                   validation_split=val_split,
                   epochs=300,
@@ -107,203 +102,55 @@ def train_and_predict_dragons(t, y_unscaled, x, targeted_regularization=True, ou
     yt_hat_test = dragonnet.predict(x_test)
     yt_hat_train = dragonnet.predict(x_train)
 
-    test_outputs += [_split_output(yt_hat_test, t_test, y_test, y_scaler, x_test, test_index)]
-    train_outputs += [_split_output(yt_hat_train, t_train, y_train, y_scaler, x_train, train_index)]
+    test_outputs = _split_output(yt_hat_test, t_test, y_test, y_scaler, x_test)
+    train_outputs = _split_output(yt_hat_train, t_train, y_train, y_scaler, x_train)
     K.clear_session()
 
     return test_outputs, train_outputs
 
 
-def train_and_predict_ned(t, y_unscaled, x, targeted_regularization=True, output_dir='',
-                          knob_loss=dragonnet_loss_binarycross, ratio=1., dragon='', val_split=0.2, batch_size=64):
-    verbose = 0
-    y_scaler = StandardScaler().fit(y_unscaled)
-    y = y_scaler.transform(y_unscaled)
-
-    train_outputs = []
-    test_outputs = []
-
-    nednet = make_ned(x.shape[1], 0.01)
-
-    metrics_ned = [ned_loss]
-    metrics_cut = [regression_loss]
-
-    # for reproducing the ihdp result
-    i = 0
-
-    tf.random.set_random_seed(i)
-    np.random.seed(i)
-
-    # change the test_size to get in sample and out sample estimates
-
-    test_size = 0.
-    train_index, test_index = train_test_split(np.arange(x.shape[0]), test_size=test_size)
-    if test_size == 0:
-        test_index = train_index
-
-    x_train, x_test = x[train_index], x[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    t_train, t_test = t[train_index], t[test_index]
-    yt_train = np.concatenate([y_train, t_train], 1)
+def create_treatment_values(y):
+    is_odd = lambda value: value % 2
+    return np.array([is_odd(value) for value in y])
 
 
-    nednet.compile(
-        optimizer=Adam(learning_rate = 1e-3),
-        loss=ned_loss, metrics=metrics_ned)
-
-    adam_callbacks = [
-        TerminateOnNaN(),
-        EarlyStopping(monitor='val_loss', patience=2, min_delta=0.),
-        ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, verbose=verbose, mode='auto',
-                          min_delta=1e-8, cooldown=0, min_lr=0)
-    ]
-
-    nednet.fit(x_train, yt_train, callbacks=adam_callbacks,
-               validation_split=val_split,
-               epochs=100,
-               batch_size=batch_size, verbose=verbose)
-
-    sgd_callbacks = [
-        TerminateOnNaN(),
-        EarlyStopping(monitor='val_loss', patience=40, min_delta=0.),
-        ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, verbose=verbose, mode='auto',
-                          min_delta=0., cooldown=0, min_lr=0)]
-
-    sgd_lr = 1e-5
-    momentum = 0.9
-    nednet.compile(optimizer=SGD(learning_rate = sgd_lr, momentum=momentum, nesterov=True), loss=ned_loss,
-                   metrics=metrics_ned)
-    print(nednet.summary())
-    nednet.fit(x_train, yt_train, callbacks=sgd_callbacks,
-               validation_split=val_split,
-               epochs=300,
-               batch_size=batch_size, verbose=verbose)
-
-    t_hat_test = nednet.predict(x_test)[:, 1]
-    t_hat_train = nednet.predict(x_train)[:, 1]
-
-    # cutting the activation layer
-    cut_net = post_cut(nednet, x.shape[1], 0.01)
-
-    cut_net.compile(
-        optimizer=Adam(learning_rate = 1e-3),
-        loss=dead_loss, metrics=metrics_cut)
-
-    adam_callbacks = [
-        TerminateOnNaN(),
-        EarlyStopping(monitor='val_loss', patience=2, min_delta=0.),
-        ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, verbose=verbose, mode='auto',
-                          min_delta=1e-8, cooldown=0, min_lr=0)
-    ]
-
-    cut_net.fit(x_train, yt_train, callbacks=adam_callbacks,
-                validation_split=val_split,
-                epochs=100,
-                batch_size=batch_size, verbose=verbose)
-
-    sgd_callbacks = [
-        TerminateOnNaN(),
-        EarlyStopping(monitor='val_loss', patience=40, min_delta=0.),
-        ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, verbose=verbose, mode='auto',
-                          min_delta=0., cooldown=0, min_lr=0)]
-
-    sgd_lr = 1e-5
-    momentum = 0.9
-    cut_net.compile(optimizer=SGD(learning_rate = sgd_lr, momentum=momentum, nesterov=True), loss=dead_loss,
-                    metrics=metrics_cut)
-
-    cut_net.fit(x_train, yt_train, callbacks=sgd_callbacks,
-                validation_split=val_split,
-                epochs=300,
-                batch_size=batch_size, verbose=verbose)
-
-    y_hat_test = cut_net.predict(x_test)
-    y_hat_train = cut_net.predict(x_train)
-
-    yt_hat_test = np.concatenate([y_hat_test, t_hat_test.reshape(-1, 1)], 1)
-    yt_hat_train = np.concatenate([y_hat_train, t_hat_train.reshape(-1, 1)], 1)
-
-    test_outputs += [_split_output(yt_hat_test, t_test, y_test, y_scaler, x_test, test_index)]
-    train_outputs += [_split_output(yt_hat_train, t_train, y_train, y_scaler, x_train, train_index)]
-    K.clear_session()
-
-    return test_outputs, train_outputs
+def create_targets(y):
+    is_prime = lambda value: value in {2, 3, 4, 7}
+    return np.array([int(is_prime(value)) for value in y])
 
 
-def run_ihdp(data_base_dir='/Users/claudiashi/data/ihdp_csv', output_dir='~/result/ihdp/',
-             knob_loss=dragonnet_loss_binarycross,
-             ratio=1., dragon=''):
+def run_mnist(output_dir, dragon, knob_loss=dragonnet_loss_binarycross, ratio=1.):
     print("the dragon is {}".format(dragon))
 
-    simulation_files = sorted(glob.glob("{}/*.csv".format(data_base_dir)))
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    t_train = create_treatment_values(y_train)
+    t_test = create_treatment_values(y_test)
+    y_train = create_targets(y_train)
+    y_test = create_targets(y_test)
 
-    for idx, simulation_file in enumerate(simulation_files):
-
-        simulation_output_dir = os.path.join(output_dir, str(idx))
-
-        os.makedirs(simulation_output_dir, exist_ok=True)
-
-        x = load_and_format_covariates_ihdp(simulation_file)
-        t, y, y_cf, mu_0, mu_1 = load_all_other_crap(simulation_file)
-        np.savez_compressed(os.path.join(simulation_output_dir, "simulation_outputs.npz"),
-                            t=t, y=y, y_cf=y_cf, mu_0=mu_0, mu_1=mu_1)
-
-        for is_targeted_regularization in [True, False]:
-            print("Is targeted regularization: {}".format(is_targeted_regularization))
-            if dragon == 'nednet':
-                test_outputs, train_output = train_and_predict_ned(t, y, x,
-                                                                   targeted_regularization=is_targeted_regularization,
-                                                                   output_dir=simulation_output_dir,
-                                                                   knob_loss=knob_loss, ratio=ratio, dragon=dragon,
-                                                                   val_split=0.2, batch_size=64)
-            else:
-
-                test_outputs, train_output = train_and_predict_dragons(t, y, x,
-                                                                       targeted_regularization=is_targeted_regularization,
-                                                                       output_dir=simulation_output_dir,
-                                                                       knob_loss=knob_loss, ratio=ratio, dragon=dragon,
-                                                                       val_split=0.2, batch_size=64)
-
-            if is_targeted_regularization:
-                train_output_dir = os.path.join(simulation_output_dir, "targeted_regularization")
-            else:
-                train_output_dir = os.path.join(simulation_output_dir, "baseline")
-            os.makedirs(train_output_dir, exist_ok=True)
-
-            # save the outputs of for each split (1 per npz file)
-            for num, output in enumerate(test_outputs):
-                np.savez_compressed(os.path.join(train_output_dir, "{}_replication_test.npz".format(num)),
-                                    **output)
-
-            for num, output in enumerate(train_output):
-                np.savez_compressed(os.path.join(train_output_dir, "{}_replication_train.npz".format(num)),
-                                    **output)
+    for is_targeted_regularization in [True, False]:
+        print("Is targeted regularization: {}".format(is_targeted_regularization))
+        test_outputs, train_output = train_and_predict_dragons(t_train, t_test, y_train, y_test, x_train, x_test,
+                                                               targeted_regularization=is_targeted_regularization,
+                                                               output_dir=output_dir,
+                                                               knob_loss=knob_loss, ratio=ratio, dragon=dragon,
+                                                               val_split=0.2, batch_size=64)
 
 
-def turn_knob(data_base_dir='/Users/claudiashi/data/test/', knob='dragonnet',
-              output_base_dir=''):
-    output_dir = os.path.join(output_base_dir, knob)
-
-    if knob == 'dragonnet':
-        run_ihdp(data_base_dir=data_base_dir, output_dir=output_dir, dragon='dragonnet')
-
-    if knob == 'tarnet':
-        run_ihdp(data_base_dir=data_base_dir, output_dir=output_dir, dragon='tarnet')
-
-    if knob == 'nednet':
-        run_ihdp(data_base_dir=data_base_dir, output_dir=output_dir, dragon='nednet')
+def turn_knob(args: argparse):
+    output_dir = os.path.join(args.output_base_dir, args.knob)
+    run_mnist(output_dir=output_dir, dragon='dragonnet')
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_base_dir', type=str, help="path to directory LBIDD")
-    parser.add_argument('--knob', type=str, default='early_stopping',
-                        help="dragonnet or tarnet or nednet")
-
-    parser.add_argument('--output_base_dir', type=str, help="directory to save the output")
+    parser.add_argument('--knob', type=str, default='dragonnet')
+    parser.add_argument('--data-base-dir', type=str, default="../dat/ihdp/csv")
+    parser.add_argument('--output-base-dir', type=str, default="../result/ihdp")
 
     args = parser.parse_args()
-    turn_knob(args.data_base_dir, args.knob, args.output_base_dir)
+    turn_knob(args)
 
 
 if __name__ == '__main__':
--- src/experiment/models.py
+++ src/experiment/models.py
@@ -29,17 +29,6 @@ def regression_loss(concat_true, concat_pred):
     return loss0 + loss1
 
 
-def ned_loss(concat_true, concat_pred):
-    t_true = concat_true[:, 1]
-
-    t_pred = concat_pred[:, 1]
-    return tf.reduce_sum(K.binary_crossentropy(t_true, t_pred))
-
-
-def dead_loss(concat_true, concat_pred):
-    return regression_loss(concat_true, concat_pred)
-
-
 def dragonnet_loss_binarycross(concat_true, concat_pred):
     return regression_loss(concat_true, concat_pred) + binary_classification_loss(concat_true, concat_pred)
 
@@ -48,7 +37,6 @@ def treatment_accuracy(concat_true, concat_pred):
     t_true = concat_true[:, 1]
     t_pred = concat_pred[:, 2]
     return binary_accuracy(t_true, t_pred)
-
 
 
 def track_epsilon(concat_true, concat_pred):
@@ -120,7 +108,6 @@ def make_dragonnet(input_dim, reg_l2):
     x = Dense(units=200, activation='elu', kernel_initializer='RandomNormal')(x)
     x = Dense(units=200, activation='elu', kernel_initializer='RandomNormal')(x)
 
-
     t_predictions = Dense(units=1, activation='sigmoid')(x)
 
     # HYPOTHESIS
@@ -132,9 +119,11 @@ def make_dragonnet(input_dim, reg_l2):
     y1_hidden = Dense(units=100, activation='elu', kernel_regularizer=regularizers.l2(reg_l2))(y1_hidden)
 
     # third
-    y0_predictions = Dense(units=1, activation=None, kernel_regularizer=regularizers.l2(reg_l2), name='y0_predictions')(
+    y0_predictions = Dense(units=1, activation='softmax', kernel_regularizer=regularizers.l2(reg_l2),
+                           name='y0_predictions')(
         y0_hidden)
-    y1_predictions = Dense(units=1, activation=None, kernel_regularizer=regularizers.l2(reg_l2), name='y1_predictions')(
+    y1_predictions = Dense(units=1, activation='softmax', kernel_regularizer=regularizers.l2(reg_l2),
+                           name='y1_predictions')(
         y1_hidden)
 
     dl = EpsilonLayer()
@@ -144,102 +133,3 @@ def make_dragonnet(input_dim, reg_l2):
     model = Model(inputs=inputs, outputs=concat_pred)
 
     return model
-
-
-def make_tarnet(input_dim, reg_l2):
-    """
-    Neural net predictive model. The dragon has three heads.
-    :param input_dim:
-    :param reg:
-    :return:
-    """
-
-    inputs = Input(shape=(input_dim,), name='input')
-
-    # representation
-    x = Dense(units=200, activation='elu', kernel_initializer='RandomNormal')(inputs)
-    x = Dense(units=200, activation='elu', kernel_initializer='RandomNormal')(x)
-    x = Dense(units=200, activation='elu', kernel_initializer='RandomNormal')(x)
-
-    t_predictions = Dense(units=1, activation='sigmoid')(inputs)
-
-    # HYPOTHESIS
-    y0_hidden = Dense(units=100, activation='elu', kernel_regularizer=regularizers.l2(reg_l2))(x)
-    y1_hidden = Dense(units=100, activation='elu', kernel_regularizer=regularizers.l2(reg_l2))(x)
-
-    # second layer
-    y0_hidden = Dense(units=100, activation='elu', kernel_regularizer=regularizers.l2(reg_l2))(y0_hidden)
-    y1_hidden = Dense(units=100, activation='elu', kernel_regularizer=regularizers.l2(reg_l2))(y1_hidden)
-
-    # third
-    y0_predictions = Dense(units=1, activation=None, kernel_regularizer=regularizers.l2(reg_l2), name='y0_predictions')(
-        y0_hidden)
-    y1_predictions = Dense(units=1, activation=None, kernel_regularizer=regularizers.l2(reg_l2), name='y1_predictions')(
-        y1_hidden)
-
-    dl = EpsilonLayer()
-    epsilons = dl(t_predictions, name='epsilon')
-    # logging.info(epsilons)
-    concat_pred = Concatenate(1)([y0_predictions, y1_predictions, t_predictions, epsilons])
-    model = Model(inputs=inputs, outputs=concat_pred)
-
-    return model
-
-
-def make_ned(input_dim, reg_l2=0.01):
-    """
-    Neural net predictive model. The dragon has three heads.
-    :param input_dim:
-    :param reg:
-    :return:
-    """
-
-    inputs = Input(shape=(input_dim,), name='input')
-
-    # representation
-    x = Dense(units=200, activation='elu', kernel_initializer='RandomNormal', name='ned_hidden1')(inputs)
-    x = Dense(units=200, activation='elu', kernel_initializer='RandomNormal', name='ned_hidden2')(x)
-    x = Dense(units=200, activation='elu', kernel_initializer='RandomNormal', name='ned_hidden3')(x)
-
-    t_predictions = Dense(units=1, activation='sigmoid', name='ned_t_activation')(x)
-    y_predictions = Dense(units=1, activation=None, name='ned_y_prediction')(x)
-
-    concat_pred = Concatenate(1)([y_predictions, t_predictions])
-
-    model = Model(inputs=inputs, outputs=concat_pred)
-    return model
-
-
-def post_cut(nednet, input_dim, reg_l2=0.01):
-    for layer in nednet.layers:
-        layer.trainable = False
-    nednet.layers.pop()
-    nednet.layers.pop()
-    nednet.layers.pop()
-
-    frozen = nednet
-
-    x = frozen.layers[-1].output
-    frozen.layers[-1].outbound_nodes = []
-    input = frozen.input
-
-    y0_hidden = Dense(units=100, activation='elu', kernel_regularizer=regularizers.l2(reg_l2), name='post_cut_y0_1')(x)
-    y1_hidden = Dense(units=100, activation='elu', kernel_regularizer=regularizers.l2(reg_l2), name='post_cut_y1_1')(x)
-
-    # second layer
-    y0_hidden = Dense(units=100, activation='elu', kernel_regularizer=regularizers.l2(reg_l2), name='post_cut_y0_2')(
-        y0_hidden)
-    y1_hidden = Dense(units=100, activation='elu', kernel_regularizer=regularizers.l2(reg_l2), name='post_cut_y1_2')(
-        y1_hidden)
-
-    # third
-    y0_predictions = Dense(units=1, activation=None, kernel_regularizer=regularizers.l2(reg_l2), name='y0_predictions')(
-        y0_hidden)
-    y1_predictions = Dense(units=1, activation=None, kernel_regularizer=regularizers.l2(reg_l2), name='y1_predictions')(
-        y1_hidden)
-
-    concat_pred = Concatenate(1)([y0_predictions, y1_predictions])
-
-    model = Model(inputs=input, outputs=concat_pred)
-    return model
-
